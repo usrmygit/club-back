6fc99ea672948282c0a8bd034ac514aab6e39f9c
MAC <mac@MACs-MacBook-Pro.local>
2025-06-12 15:08:21 -0700

    final commit

--- server.js
+++ server.js
@@ -26,16 +26,9 @@ app.use(
     ],
     methods: ["GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
-
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Access-Control-Allow-Methods",
-      "Access-Control-Request-Headers",
-      "Access-Control-Allow-Origin",
-    ],
   })
 );
+
 app.get("/", async (req, res) => {
   res.send("");
 });
